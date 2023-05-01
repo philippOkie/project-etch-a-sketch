a83e1a97b609ac4e2640e8e9fa8978e4f21b0837
philippOkie <kriukphilipp@gmail.com>
2023-05-01 13:46:32 +0300

    remove old divs func was added, buttons now react on the mouse, func getScale was added

--- code.js
+++ code.js
@@ -1,11 +1,43 @@
-let numberCells = prompt()
-if (numberCells > 90){
-    numberCells = prompt("number should be less than 90")
-}
+let numberCells = 64
 let amountOfCellsXY = numberCells * numberCells
 let scale = (800 / numberCells) 
 sale = scale - 1
 const containerCell = document.getElementById("containerCell")
+const big = document.getElementById("bigButton")
+const mdm = document.getElementById("mdmButton")
+const small = document.getElementById("smallButton")
+const clean = document.getElementById("cleanButton")
+
+
+// function removes old divs 
+function removeDivs() {
+    let oldDivs = document.querySelectorAll(".cell")
+
+    for (let j = 0; j < oldDivs.length; j++) {
+        oldDivs[j].parentNode.removeChild(oldDivs[j])
+    }
+}
+
+clean.addEventListener('click', () => {
+    removeDivs()
+})
+
+
+
+
+// getting player choice by html buttons and changing the size of the div container
+big.addEventListener('click', () => {
+    removeDivs()
+    makeField()
+})
+mdm.addEventListener('click', () => {
+    removeDivs()
+    makeField()
+})
+small.addEventListener('click', () => {
+    removeDivs()
+    makeField()
+})
 
 // print the number of cells in the container of cells
 function makeField(){
@@ -18,8 +50,15 @@ function makeField(){
         containerCell.appendChild(newCell)
     }
 }
+
+
+function getScale(numberCells) {
+    let amountOfCellsXY = numberCells * numberCells
+    let scale = (800 / numberCells) 
+    return sale = scale - 1, amountOfCellsXY
+}
   
-makeField()
+
 console.log(scale)
 console.log(amountOfCellsXY)
 console.log(numberCells)
